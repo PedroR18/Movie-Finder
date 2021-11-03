bc955724941c10f1ccf286c11431e2c25e82dba5
Pedro Ribeiro <poupas18@gmail.com>
2021-11-03 14:56:04 +0000

    .

--- components/Search.js
+++ components/Search.js
@@ -1,10 +1,32 @@
+import styles from '../styles/search.module.css';
 export default function Search(props) {
   return (
-    <>
-      <button onClick={() => props.toggleContent(true)}>Movies</button>
-      <button onClick={() => props.toggleContent(false)}>TV Series</button>
-      <input type="text" onChange={props.onChange} value={props.searchQuery} />
+    <div className={styles.container}>
+      <div className={styles.switchButton}>
+        <button
+          className={
+            props.contentType ? styles.activeMoviesButton : styles.moviesButton
+          }
+          onClick={() => props.toggleContent(true)}
+        >
+          Movies
+        </button>
+        <button
+          className={
+            props.contentType ? styles.seriesButton : styles.activeSeriesButton
+          }
+          onClick={() => props.toggleContent(false)}
+        >
+          TV Series
+        </button>
+      </div>
+      <input
+        onChange={props.onChange}
+        value={props.searchQuery}
+        className={styles.searchBar}
+      />
+
       <button onClick={props.generateRecommendations}>Results</button>
-    </>
+    </div>
   );
 }
