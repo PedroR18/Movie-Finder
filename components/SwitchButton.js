3cbe01e407401b31de30b4bebcb19d60c65b48ad
Pedro Ribeiro <poupas18@gmail.com>
2021-11-13 15:57:42 +0000

    .

--- components/SwitchButton.js
+++ components/SwitchButton.js
@@ -1,34 +1,85 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
 export default function SwitchButton({ contentType, toggleContent }) {
+  const [menuClasses, setMenuClasses] = useState('button');
+  const [containerClasses, setContainerClasses] = useState('navContainer');
+  const handleMenu = () => {
+    menuClasses.includes('hidden')
+      ? setMenuClasses('button')
+      : setMenuClasses('button hidden');
+
+    menuClasses.includes('hidden')
+      ? setContainerClasses('navContainer background')
+      : setContainerClasses('navContainer');
+  };
   return (
     <>
-      <div className={'container'}>
-        <div onClick={() => toggleContent(true)} className={'button'}>
-          <span className={contentType ? 'text selected' : 'text noSelected'}>
-            MOVIES
-          </span>
+      <div className={containerClasses}>
+        <div className={'burger'} onClick={() => handleMenu()}>
+          <Image
+            src="/../public/Menu.svg"
+            alt="Loading"
+            height="20px"
+            width="20px"
+          />
         </div>
+        <div className={'buttonsContainer'}>
+          <div
+            onClick={() => {
+              toggleContent(true);
+              setMenuClasses('button hidden');
+              setContainerClasses('navContainer');
+            }}
+            className={menuClasses}
+          >
+            <span className={contentType ? 'text selected' : 'text noSelected'}>
+              MOVIES
+            </span>
+          </div>
 
-        <div onClick={() => toggleContent(false)} className={'button'}>
-          <span className={!contentType ? 'text selected' : 'text noSelected'}>
-            SERIES
-          </span>
+          <div
+            onClick={() => {
+              toggleContent(false);
+              setMenuClasses('button hidden');
+              setContainerClasses('navContainer');
+            }}
+            className={menuClasses}
+          >
+            <span
+              className={!contentType ? 'text selected' : 'text noSelected'}
+            >
+              SERIES
+            </span>
+          </div>
         </div>
       </div>
       <style jsx>{`
-        .container {
+        .navContainer {
           position: fixed;
           z-index: 100;
-          right: 20px;
-          top: 11%;
-          cursor: pointer;
+          right: 15px;
+          top: 20px;
+
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .background {
+          background: url('//s2.svgbox.net/pen-brushes.svg?ic=brush-3&color=ffffff');
+          background-position: right 30px;
         }
 
         .text {
           writing-mode: vertical-rl;
-          letter-spacing: 0.8em;
-          font-size: 2em;
+          letter-spacing: 0.9em;
+          font-size: 1.7em;
           font-family: 'Teko';
           transition: ease-in 0.2s;
+          color: black;
+          text-shadow: 0px 0px 18px rgb(150, 170, 150);
         }
 
         .selected {
@@ -40,7 +91,95 @@ export default function SwitchButton({ contentType, toggleContent }) {
         }
 
         .button {
-          margin-bottom: 40px;
+          margin-bottom: 50px;
+          transition: ease-in 0.2s;
+          opacity: 1;
+          cursor: pointer;
+        }
+
+        .burger {
+          margin-bottom: 70px;
+          height: 55px;
+          width: 55px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 50px;
+          border: 1px solid white;
+          background-color: white;
+          transition: ease-in 0.2s;
+          cursor: pointer;
+        }
+
+        .burger:hover {
+          transform: scale(1.1);
+        }
+
+        .hidden {
+          visibility: hidden;
+          opacity: 0;
+        }
+
+        @media screen and (min-width: 600px) {
+          .navContainer {
+            right: 25px;
+          }
+        }
+
+        @media screen and (min-width: 700px) {
+          .text {
+            font-size: 1.9em;
+          }
+        }
+
+        @media screen and (min-width: 850px) {
+          .navContainer {
+            right: 35px;
+          }
+        }
+
+        @media screen and (min-width: 1000px) {
+          .burger {
+            visibility: hidden;
+            cursor: auto;
+          }
+
+          .background {
+            background: none;
+          }
+          .text {
+            color: white;
+            font-size: 2.3em;
+          }
+
+          .button {
+            visibility: visible !important;
+            opacity: 1 !important;
+          }
+
+          .navContainer {
+            right: 50px;
+          }
+        }
+
+        @media screen and (min-width: 1600px) {
+          .text {
+            font-size: 2.5em;
+          }
+
+          .navContainer {
+            right: 70px;
+          }
+        }
+
+        @media screen and (min-width: 2000px) {
+          .text {
+            font-size: 3em;
+          }
+
+          .navContainer {
+            right: 70px;
+          }
         }
       `}</style>
     </>
